5c539e9a2221f9853f83ac7e8bb01b4ed5030dc6
Unknown <957824770@qq.com>
2019-04-08 18:47:44 +0800

    UserChallengingProblemFilter iscontest

--- apps/user_operation/filters.py
+++ apps/user_operation/filters.py
@@ -1,5 +1,4 @@
 from django_filters import rest_framework
-import django_filters
 from .models import UserCollect, UserAcceptedProblems, UserChallengingProblems
 from Utlis.NullFilter import NullFilter
 
@@ -12,14 +11,15 @@ class UserCollectFilter(rest_framework.FilterSet):
 
 class UserAcceptedProblemFilter(rest_framework.FilterSet):
     iscontest = NullFilter(field_name="problem__contest")
-    problem__contest = django_filters.NumberFilter()
 
     class Meta:
         model = UserAcceptedProblems
-        fields = ['user', 'iscontest', 'problem__contest']
+        fields = ['user', 'iscontest']
 
 
 class UserChallengingProblemFilter(rest_framework.FilterSet):
+    iscontest = NullFilter(field_name="problem__contest")
+
     class Meta:
         model = UserChallengingProblems
-        fields = ['user']
+        fields = ['user', 'iscontest']
