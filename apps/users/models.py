4b37ee4dd8a9afa5fecc57c26209555ef9b88c8e
Unknown <957824770@qq.com>
2019-04-19 23:57:45 +0800

    UserProfile verbose_name

--- apps/users/models.py
+++ apps/users/models.py
@@ -20,13 +20,39 @@ class User(AbstractUser):
 
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        blank=True,
+        verbose_name="用户")
     avatar = models.ImageField(
+        blank=True,
         upload_to='avatars/',
-        default='avatars/default.jpg')
-    accepted_num = models.IntegerField(default=0)
-    submission_num = models.IntegerField(default=0)
-    ranking = models.IntegerField(default=0)
+        default='avatars/default.jpg', verbose_name="头像")
+    real_name = models.CharField(
+        max_length=30,
+        null=True,
+        blank=True,
+        verbose_name="真实姓名")
+    student_no = models.IntegerField(
+        unique=True,
+        null=True,
+        blank=True,
+        verbose_name="学号")
+    mood = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True,
+        verbose_name="签名")
+    QQ = models.IntegerField(null=True, blank=True, verbose_name="QQ")
+    blog = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True,
+        verbose_name="博客")
+    accepted_num = models.IntegerField(default=0, verbose_name="通过数量")
+    submission_num = models.IntegerField(default=0, verbose_name="提交数量")
+    ranking = models.IntegerField(default=0, verbose_name="排名")
 
     def add_submission_number(self):
         self.submission_num = models.F("submission_num") + 1
